fd2add526d82208f3b1fcdcdbfaf8c2cbf987209
Nori <loveandfight2186@yahoo.co.jp>
2021-12-22 17:10:09 -0800

    create first article factory rspec test

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    slug { "MyString" }
+    title { "Sample article" }
+    content { "Sample Content abc" }
+    slug { "sample-article" }
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'tests article object' do
+    #expect(1).to be_positive
+    #expect(5).to be > 3
+    #article = Article.create({title:"Sample article", content:"aaa"})
+
+    article = create(:article)
+
+    expect(article.title).to eq("Sample article")
+  end
 end
